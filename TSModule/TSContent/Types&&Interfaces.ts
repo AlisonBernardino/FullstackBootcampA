165c428323053d6e310ae789c54c8e245bff4e21
Alison Fullstack <alisontaqu@live.com>
2022-07-16 23:59:10 -0300

    Typescript project, types and interfaces codes added

---
+++ TSModule/TSContent/Types&&Interfaces.ts
@@ -0,0 +1,70 @@
+// Interfaces
+// >>> Samples
+interface droneInterface {
+  type: string;
+  modality: 'Air' | 'Water';
+  showDroneTypeMessage(outputMessage: string): void;
+  isMultiPartsCompatible: boolean;
+}
+
+interface camoDrones extends droneInterface {
+  camouflageMainColor: string;
+  camouflageSecondaryColor: string;
+  isMultiPartsCompatible: true;
+}
+
+// Types
+type specialDeviceType = camoDrones | extraSpecs;
+
+interface extraSpecs extends droneInterface {
+  isExtraStorageAvailable: boolean;
+  isControlAmplifiedRangeAvailable: boolean;
+  extraStorageCapacity: string;
+  cameraQuality: string;
+  amplifiedRangeCapacity: string;
+}
+
+const droneA: droneInterface = {
+  type: 'AX02',
+  modality: 'Air',
+  isMultiPartsCompatible: true,
+  showDroneTypeMessage: (outputMessage) => `${outputMessage}.`,
+};
+
+droneA.showDroneTypeMessage(`${droneA.type}`);
+
+const droneB: droneInterface = {
+  type: 'WX01',
+  modality: 'Water',
+  isMultiPartsCompatible: true,
+  showDroneTypeMessage: (outputMessage) => `${outputMessage}`,
+};
+
+droneB.showDroneTypeMessage(`${droneB.type}`);
+
+const droneC: extraSpecs = {
+  type: 'AM003',
+  modality: 'Air',
+  isMultiPartsCompatible: true,
+  isExtraStorageAvailable: true,
+  isControlAmplifiedRangeAvailable: true,
+  extraStorageCapacity: '2TB',
+  cameraQuality: 'Ultra HD - 8K',
+  amplifiedRangeCapacity: '700m',
+  showDroneTypeMessage: (outputMessage) => `${outputMessage}`,
+};
+
+droneC.showDroneTypeMessage(`${droneC.type}`);
+
+const droneD: specialDeviceType = {
+    
+}
+
+// Grabbing and handling the HTML "input" element
+const firstTitleSelection = document.getElementById(
+  'firstTitleField'
+) as HTMLInputElement;
+firstTitleSelection.addEventListener('input', (event) => {
+  const selectionItem = event.currentTarget as HTMLInputElement;
+  console.log(selectionItem.value);
+});
---
+++ TSModule/TSContent/projectIndexTSVersion.ts
@@ -0,0 +1,8 @@
+function sumValues(valueA: number, valueB: number){
+    const valuesSum = valueA + valueB;
+    return valuesSum;
+}
+
+console.log(sumValues(1, 3));
+
+
